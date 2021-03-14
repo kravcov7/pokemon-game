e379defc7bd6bfb3307c3d801d43852a2b6c2e83
Artem <dr.artem-k@yandex.ru>
2021-03-14 22:57:17 +0400

    название файлов

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Header from "./Header/index";
-import Layout from "./Layout/index";
-import Footer from "./Footer/index";
+import Header from "../components/Header/index";
+import Layout from "../components/Layout/index";
+import Footer from "../components/Footer/index";
 import Logo from "./../assets/bg3.jpg";
 import PokemonCard from "./PokemonCard";
 
