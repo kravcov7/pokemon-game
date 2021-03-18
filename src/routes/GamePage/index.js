377da9a4572e96ebdf93b85c0fc4ca632816a9c7
Artem <dr.artem-k@yandex.ru>
2021-03-18 16:18:06 +0400

    вывел покемонов на GamePage

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -1,4 +1,10 @@
 import { useHistory } from 'react-router-dom';
+import {useState} from 'react';
+
+import POKEMONS from '../../assets/pokemon.json';
+import PokemonCard from '../../components/PokemonCard';
+import Layout from '../../components/Layout';
+
 import d from "./style.module.css";
 
 const GamePage = () => {
@@ -6,8 +12,20 @@ const GamePage = () => {
   const handleClick = () => {    
     history.push('/');
   }
+  const pokemons = POKEMONS;
+  const [isData, setData] = useState(pokemons);
+  const choiceCard = (id) => {
+    const res =isData.filter(item => {
+        if(item.id === Number(id)) {
+            item["active"] = true
+        }
+        return true
+    });
+    setData(res)
+}
 
   return (
+    <>
     <header className={d.root}>
       <div className={d.forest}></div>
       <div className={d.container}>
@@ -15,6 +33,23 @@ const GamePage = () => {
         <button onClick={handleClick}>Home Page</button>
       </div>
     </header>
+    <Layout colorBg='#181d23'>
+      <div className={d.flex}>
+        {
+          isData.map(item => <PokemonCard
+            key={item.id}
+            name={item.name}
+            type={item.type}
+            values={item.values}
+            img={item.img}
+            id={item.id}
+            choice={choiceCard}
+            active={item.active}
+        />)
+        }
+      </div>
+    </Layout>
+    </>
   );
 };
 
