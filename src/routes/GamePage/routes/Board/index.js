c30e13bda899be1f4fa316deff7712d3024847f3
Artem <dr.artem-k@yandex.ru>
2021-03-24 00:06:46 +0400

    add pokemons on board

--- src/routes/GamePage/routes/Board/index.js
+++ src/routes/GamePage/routes/Board/index.js
@@ -1,24 +1,30 @@
-import s from './style.module.css';
+import s from "./style.module.css";
+import { useContext } from "react";
+import { PokemonContext } from "../../../../context/pokemonContext";
+import PokemonCard from "../../../../components/PokemonCard";
 
 const BoardPage = () => {
-    return (
-        <div className={s.root}>
-						<div className={s.playerOne}>
-
-						</div>
-            <div className={s.board}>
-                <div className={s.boardPlate}>1</div>
-                <div className={s.boardPlate}>2</div>
-                <div className={s.boardPlate}>3</div>
-                <div className={s.boardPlate}>4</div>
-                <div className={s.boardPlate}>5</div>
-                <div className={s.boardPlate}>6</div>
-                <div className={s.boardPlate}>7</div>
-                <div className={s.boardPlate}>8</div>
-                <div className={s.boardPlate}>9</div>
-            </div>
-        </div>
-    );
+  const { pokemons } = useContext(PokemonContext);
+  return (
+    <div className={s.root}>
+      <div className={s.playerOne}>
+        {Object.values(pokemons).map(({ id, name, type, values, img }) => (
+          <PokemonCard className={s.card} key={id} name={name} type={type} values={values} img={img} id={id} minimize isActive />
+        ))}
+      </div>
+      <div className={s.board}>
+        <div className={s.boardPlate}>1</div>
+        <div className={s.boardPlate}>2</div>
+        <div className={s.boardPlate}>3</div>
+        <div className={s.boardPlate}>4</div>
+        <div className={s.boardPlate}>5</div>
+        <div className={s.boardPlate}>6</div>
+        <div className={s.boardPlate}>7</div>
+        <div className={s.boardPlate}>8</div>
+        <div className={s.boardPlate}>9</div>
+      </div>
+    </div>
+  );
 };
 
 export default BoardPage;
