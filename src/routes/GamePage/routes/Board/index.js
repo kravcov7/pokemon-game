d98af4ba69d66b8f1f757c22a56632689ef3b5ce
Artem <dr.artem-k@yandex.ru>
2021-03-25 17:28:52 +0400

    add player2

--- src/routes/GamePage/routes/Board/index.js
+++ src/routes/GamePage/routes/Board/index.js
@@ -1,15 +1,34 @@
 import s from "./style.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useImperativeHandle, useState } from "react";
 import { useHistory } from 'react-router-dom';
 import { PokemonContext } from "../../../../context/pokemonContext";
 import PokemonCard from "../../../../components/PokemonCard";
 
 const BoardPage = () => {
+  const [board, setBoard] = useState([]);
+  const [player2, setPlayer2] = useState([]);
   const { pokemons } = useContext(PokemonContext);
   const history = useHistory();
+  console.log('### board', player2);
 
-  if(Object.keys(pokemons).length === 0) {
-    history.replace('/game');
+  useEffect(async () => {
+    const boardResponce = await fetch('https://reactmarathon-api.netlify.app/api/board');
+    const boardRequest = await boardResponce.json();
+
+    setBoard(boardRequest.data);
+
+    const player2Responce = await fetch('https://reactmarathon-api.netlify.app/api/create-player');   
+    const player2Request = await player2Responce.json(); 
+
+    setPlayer2(player2Request.data);
+  }, [])
+
+  // if(Object.keys(pokemons).length === 0) {
+  //   history.replace('/game');
+  // }
+
+  const handleClickBoardPlate = (position) => {
+    console.log('### position', position);
   }
 
 
@@ -21,15 +40,24 @@ const BoardPage = () => {
         ))}
       </div>
       <div className={s.board}>
-        <div className={s.boardPlate}>1</div>
-        <div className={s.boardPlate}>2</div>
-        <div className={s.boardPlate}>3</div>
-        <div className={s.boardPlate}>4</div>
-        <div className={s.boardPlate}>5</div>
-        <div className={s.boardPlate}>6</div>
-        <div className={s.boardPlate}>7</div>
-        <div className={s.boardPlate}>8</div>
-        <div className={s.boardPlate}>9</div>
+        {
+          board.map(item => (
+          <div key={item.position} className={s.boardPlate} onClick={() => !item.card && handleClickBoardPlate(item.position)} >
+            {
+              item.card && <PokemonCard {...item} monomaze />
+            }
+
+          </div>))
+        }
+        
+      </div>
+      <div className={s.playerTwo}>
+        {
+          player2.map(({ id, name, type, values, img }) => (
+            <PokemonCard className={s.card} key={id} name={name} type={type} values={values} img={img} id={id} minimize isActive />
+          ))
+        }
+
       </div>
     </div>
   );
