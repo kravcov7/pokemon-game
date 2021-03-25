33f34e452cd65084920f054294bca6fb0312313b
Artem <dr.artem-k@yandex.ru>
2021-03-25 23:41:00 +0400

    создал FinishPage

--- src/routes/GamePage/routes/Finish/index.js
+++ src/routes/GamePage/routes/Finish/index.js
@@ -1,8 +1,36 @@
-const FinishPage = () => {
-  return (
-    <div>
-      This is FinishPage
+import { useHistory } from "react-router-dom";
+import { useContext } from "react";
+import { PokemonContext } from "../../../../context/pokemonContext";
+import { BoardContext } from "../../../../context/boardContext";
+import s from "./style.module.css";
+import PokemonCard from "../../../../components/PokemonCard";
 
+const FinishPage = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.replace("/game");
+  };
+
+  const { pokemons } = useContext(PokemonContext);
+
+  const value = useContext(BoardContext); 
+
+  return (
+    <div className={s.container}>
+      <section className={s.player1}>
+        {Object.values(pokemons).map((item) => (
+          <PokemonCard key={item.key} type={item.type} name={item.name} img={item.img} id={item.id} values={item.values} isActive />
+        ))}
+      </section>
+      <button className={s.button} onClick={handleClick}>
+        END GAME
+      </button>
+      <section className={s.player2}>
+        {Object.values(pokemons).map((item) => (
+          <PokemonCard key={item.key} type={item.type} name={item.name} img={item.img} id={item.id} values={item.values} isActive />
+        ))}
+      </section>
     </div>
   );
 };
